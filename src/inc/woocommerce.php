79fa757c8f1efb05730006f03c35e89333e260bc
luisfer.uio <fsuarez.an@gmail.com>
2025-03-29 20:39:48 -0500

    version 3 de woocommerce.php mas css y js

--- src/inc/woocommerce.php
+++ src/inc/woocommerce.php
@@ -1,37 +1,44 @@
 <?php
-// Agrega el campo de video en WooCommerce
-function cf_stream_add_video_field() {
-    woocommerce_wp_text_input(array(
-        'id'          => '_cf_stream_video_id',
-        'label'       => __('Cloudflare Video ID', 'woocommerce'),
-        'desc_tip'    => true,
-        'description' => __('Ingresa la ID del video de Cloudflare Stream.', 'woocommerce'),
-    ));
-}
-add_action('woocommerce_product_options_general_product_data', 'cf_stream_add_video_field');
+// Agregar el video a la galería de imágenes del producto en WooCommerce
+function cf_stream_add_video_to_gallery($attachment_ids, $product) {
+    $video_id = get_post_meta($product->get_id(), '_cf_stream_video_id', true);
 
-// Guarda la ID del video
-function cf_stream_save_video_field($post_id) {
-    $video_id = isset($_POST['_cf_stream_video_id']) ? sanitize_text_field($_POST['_cf_stream_video_id']) : '';
-    update_post_meta($post_id, '_cf_stream_video_id', $video_id);
-}
-add_action('woocommerce_process_product_meta', 'cf_stream_save_video_field');
+    if ($video_id) {
+        // Generar una imagen de miniatura del video (asumimos que Cloudflare tiene un endpoint para esto)
+        $thumbnail_url = "https://videodelivery.net/{$video_id}/thumbnails/thumbnail.jpg";
 
-// Agregar el video como una imagen en la galería de productos
-function cf_stream_add_video_to_gallery($html, $attachment_id) {
-    global $post;
-    $video_id = get_post_meta($post->ID, '_cf_stream_video_id', true);
+        // Crear un array simulando un attachment de imagen
+        $video_attachment = [
+            'id' => "cf_video_{$video_id}",
+            'src' => $thumbnail_url,
+            'full_src' => $thumbnail_url,
+            'thumbnail' => $thumbnail_url,
+            'srcset' => '',
+            'sizes' => '',
+        ];
 
-    if (!empty($video_id)) {
-        $video_thumbnail = 'URL_DE_LA_IMAGEN_PREDETERMINADA_DEL_VIDEO'; // Reemplaza con una imagen representativa
-        $video_url = 'https://customer-xd1ajhckvre3vwhu.cloudflarestream.com/' . esc_attr($video_id) . '/iframe';
-
-        $html .= '<div class="woocommerce-product-gallery__image">';
-        $html .= '<a href="' . esc_url($video_url) . '" class="woocommerce-product-video" target="_blank">';
-        $html .= '<img src="' . esc_url($video_thumbnail) . '" alt="Video Preview">';
-        $html .= '</a>';
-        $html .= '</div>';
+        // Agregar el video a la galería
+        array_unshift($attachment_ids, $video_attachment);
     }
-    return $html;
+
+    return $attachment_ids;
 }
-add_filter('woocommerce_single_product_image_thumbnail_html', 'cf_stream_add_video_to_gallery', 10, 2);
+add_filter('woocommerce_product_get_gallery_image_ids', 'cf_stream_add_video_to_gallery', 10, 2);
+
+// Agregar script para abrir el video en un modal al hacer clic
+function cf_stream_enqueue_scripts() {
+    wp_enqueue_script('cf-stream-video-lightbox', plugin_dir_url(__FILE__) . 'js/cf-stream-lightbox.js', ['jquery'], null, true);
+    wp_enqueue_style('cf-stream-video-lightbox', plugin_dir_url(__FILE__) . 'css/cf-stream-lightbox.css');
+}
+add_action('wp_enqueue_scripts', 'cf_stream_enqueue_scripts');
+
+// Agregar el modal HTML al pie de la página
+function cf_stream_add_video_modal() {
+    echo '<div id="cf-stream-video-modal" class="cf-stream-modal">
+            <div class="cf-stream-modal-content">
+                <span class="cf-stream-close">&times;</span>
+                <iframe id="cf-stream-video-frame" src="" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </div>
+          </div>';
+}
+add_action('wp_footer', 'cf_stream_add_video_modal');
